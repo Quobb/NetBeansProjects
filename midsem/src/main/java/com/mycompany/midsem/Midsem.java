a31909d91b803989b0f9e2e05f984603f2f84bb7
Malware <malware@kali>
2023-04-10 03:49:23 +0000

    first commit

---
+++ midsem/src/main/java/com/mycompany/midsem/Midsem.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.midsem;
+
+/**
+ *
+ * @author malware
+ */
+public class Midsem {
+
+   
+    
+}
+ class Utility{
+        protected double water;
+        protected double electricity;
+       
+        public Utility(){
+        water = 0;
+        electricity = 0;
+        }
+        public Utility(double water ,double electricity){
+           this.water = water;
+           this.electricity = electricity;
+        }
+        
+        public double getWater(){
+         return water;
+        }
+        
+        public double getElectricity(){
+          return electricity;
+        }
+        public double waterBill(){
+            if (water <= 500) {
+                return 0.0;   
+            }
+            else{
+                return  (water - 500)*1.05;
+            }
+        }
+       
+    }
+class myBill extends Utility{
+     public myBill(){
+     super(0,0);
+     }
+     public myBill(double water, double electricity){
+     this.electricity = electricity;
+     this.water = water;   
+     }
+     public double electricityBill(){
+
+        if (electricity <= 50){
+             return 0.0;
+         } else if (electricity > 50) {
+             return (electricity - 50)*2.5;
+         } else if(electricity > 100)   {
+             return (50 * 5.7)+ (electricity - 50);
+         }
+        return (0);
+     }
+     public double totalBill(){
+         return  electricityBill() + waterBill();
+     }
+}
